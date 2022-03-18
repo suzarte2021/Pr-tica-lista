f8b8edac07abd0829de24e30448fe77323a6e5db
suzarte2021 <84162098+suzarte2021@users.noreply.github.com>
2022-03-18 19:28:44 -0300

    Listar 10 números inteiros com Arrays

---
+++ Arrays.java
@@ -0,0 +1,26 @@
+package Arrays;
+
+public class Arrays {
+
+	public static void main(String[] args) {
+		//Prática lista: Listar 10 números inteiros com arrays
+
+		int numero = 10;
+
+		int arrays [] = new  int [numero];
+
+		for (int i = 0; i < numero; i++) {
+
+			arrays [i] =i;
+
+			System.out.println(arrays[i]+ " ");
+
+		}
+
+	}
+
+
+
+	}
+
+
